2dbb2f5d0b4c8394a5d36c2a55b13474520be5ab
Jaron Earle <jaronaearle@gmail.com>
2022-11-23 00:32:10 -0700

    refactor: move start session into main

--- internal/bot/DiscordBot.go
+++ internal/bot/DiscordBot.go
@@ -2,9 +2,6 @@ package bot
 
 import (
 	"fmt"
-	"os"
-	"os/signal"
-	"syscall"
 
 	"github.com/bwmarrin/discordgo"
 )
@@ -25,51 +22,17 @@ func (db *DiscordBot) RegisterHandlers() {
 	db.Session.Identify.Intents = discordgo.IntentGuildMessages
 }
 
-func (db *DiscordBot) StartBotSession() {
-	err := db.Session.Open()
-	if err != nil {
-		fmt.Println("StartBotSession: Error opening discord bot connection:", err)
-		return
-	}
-
-	fmt.Println("Captain Hook is running...")
-	sc := make(chan os.Signal, 1)
-	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
-	<-sc
-
-	defer db.Session.Close()
-}
-
-func messageCreate (s *discordgo.Session, m *discordgo.MessageCreate) {
+func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 
-	emb := &discordgo.MessageEmbed{
-		URL: "https://utahavalanchecenter.org/sites/default/files/forecast/202211/20221119-061743-6.png",
-		Title: "Avy Report: November 20, 2022",
-		Description: "Generally safe avalanche conditions.\nSmall avalanches are possible in isolated areas or extreme terrain.\n Remember that risk is inherent in mountain travel.\n\nWe are transitioning back to intermittent updates.  The next update will be on Wednesday.",
-		Image: &discordgo.MessageEmbedImage{
-			URL: "https://utahavalanchecenter.org/sites/default/files/forecast/202211/20221119-061743-6.png",
-			Width: 50,
-			Height: 50,
-		},
-	}
-
+	// TODO: access channel messages for chat op things
 	if m.Content == "" {
 		msgs, err := s.ChannelMessages(m.ChannelID, 1, "", "", m.ID)
 		if err != nil {
 			fmt.Printf("messageCreate: Error getting channel messages: %v", err)
 		}
 		fmt.Println(msgs)
-
-	}
-
-	if m.Content == "" {
-		_, err := s.ChannelMessageSend(m.ChannelID, "Hello from skele")
-		s.ChannelMessageSendEmbed(m.ChannelID, emb)
-		if err != nil {
-			fmt.Printf("messageCreate: Error sending message: %v", err)
-		}
 	}
 }
