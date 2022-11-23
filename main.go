d7096f0fcf87bd6d0abe64846d80818e21d8cd4d
Jaron Earle <jaronaearle@gmail.com>
2022-11-23 00:39:20 -0700

    feat/refactor: add bot & cron start funcs, mostly make it work, a few todo notes

--- main.go
+++ main.go
@@ -1,15 +1,26 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"os"
+	"os/signal"
 	"skele/config"
 	"skele/internal/bot"
 	"skele/internal/crawlers"
 	"skele/internal/handlers"
+	"syscall"
+	"time"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-co-op/gocron"
 	"github.com/gocolly/colly"
 )
+
+var (
+	avyCenterDomains = []string{"https://utahavalanchecenter.org/", "https://utahavalanchecenter.org", "utahavalanchecenter.org/", "www.utahavalanchecenter.org/", "utahavalanchecenter.org"}
+)
+
 type Payload struct {
 	Content string `json:"content"`
 }
@@ -20,71 +31,72 @@ func main() {
 		fmt.Printf("main: Error initializing config: %v", err)
 	}
 
-	c := colly.NewCollector(colly.AllowedDomains("https://utahavalanchecenter.org/","https://utahavalanchecenter.org", "utahavalanchecenter.org/", "www.utahavalanchecenter.org/", "utahavalanchecenter.org"))
+	c := colly.NewCollector(colly.AllowedDomains(avyCenterDomains...))
 	ac := crawlers.NewAvyCrawler(c)
 
-	ds, err := discordgo.New(cfg.BotToken)
+	session, err := discordgo.New(cfg.BotToken)
 	if err != nil {
 		panic(err)
 	}
 
-	db := bot.NewDiscordBot(ds)
-	db.RegisterHandlers()
-	db.StartBotSession()
-
+	bot := bot.NewDiscordBot(session)
 	avyReportHandler := handlers.AvyReportHandler{
 		AvyCrawler: ac,
-		DiscordBot: db,
+		DiscordBot: bot,
 	}
 
-	avyReportHandler.SendAvyReport()
+	ctx, cancel := context.WithCancel((context.Background()))
 
-	// data := Payload{
-	// 	Content: "From cron, with <3",
+	go startCron(ctx, avyReportHandler, cancel)
+	go startBot(ctx, bot, cancel)
+
+	sig := make(chan os.Signal, 3)
+	signal.Notify(sig, syscall.SIGTERM, syscall.SIGINT, syscall.SIGHUP, os.Interrupt)
+
+	<-sig
+
+	// TODO: make contexts/cancel work
+	// select {
+	// case <-sig:
+	// case <-ctx.Done():
+	// 	return
 	// }
-	// s := gocron.NewScheduler(time.UTC)
-
-	// s.Every(7).Seconds().Do(func() {
-	// 	postTest(data)
-	// })
-
-	// s.StartBlocking()
-
-	// err := postTest(data)
-	// if err != nil {
-	// 	panic(err)
-	// }
+	// cancel()
+	fmt.Println("Gracefully returning to the grave...")
 }
 
+func startCron(pCtx context.Context, a handlers.AvyReportHandler, exit context.CancelFunc) {
+	fmt.Println("Starting cron...")
+	defer exit()
+	defer fmt.Println("Exiting cron...")
 
+	mtnTZ, err := time.LoadLocation("America/Denver")
+	if err != nil {
+		fmt.Printf("Error loading america/denver tz: %s", err)
+		return
+	}
+	s := gocron.NewScheduler(mtnTZ)
 
-// func postTest(p Payload) (err error) {
-// 	pBytes, err := json.Marshal(p)
-// 	if err != nil {
-// 		fmt.Printf("postTest: Error marshalling payload: %v", err)
-// 		return
-// 	}
+	s.Every(1).Days().At("07:30").Do(func() {
+		a.SendAvyReport()
+	})
 
-// 	body := bytes.NewReader(pBytes)
+	s.StartBlocking()
 
-// 	req, err := http.NewRequest("POST", "https://discord.com/api/webhooks/1043741007398846604/wwGGUlBiV_OLKS-m3Xn0TVpM-dayDb5X48BXRuyBgKvKdAmrtZQCV9Jd_kO8hdqHJwT6", body)
-// 	if err != nil {
-// 		fmt.Printf("postTest: Error building new request: %v", err)
-// 		return err
-// 	}
+	// TODO: make contexts work
+	// <- pCtx.Done()
+}
 
-// 	req.Header.Set("Accept", "application/json")
-// 	req.Header.Set("Content-Type", "application/json")
+func startBot(pCtx context.Context, bot *bot.DiscordBot, exit context.CancelFunc) {
+	fmt.Println("Starting bot session...")
+	defer exit()
+	defer fmt.Println("Exiting bot session...")
 
-// 	resp, err := http.DefaultClient.Do(req)
-// 	if err != nil {
-// 		fmt.Printf("postTest: Error sending request: %v", err)
-// 		return err
-// 	}
-// 	fmt.Println("Request sent")
-// 	defer resp.Body.Close()
+	bot.RegisterHandlers()
 
-// 	return nil
-// }
-
-// curl -i -H "Accept: application/json" -H "Content-Type:application/json" -X POST --data "{\"content\": \"Posted Via Command line\"}" https://discord.com/api/webhooks/1043741007398846604/wwGGUlBiV_OLKS-m3Xn0TVpM-dayDb5X48BXRuyBgKvKdAmrtZQCV9Jd_kO8hdqHJwT6
+	err := bot.Session.Open()
+	if err != nil {
+		fmt.Printf("startBot: Error opening websocket connection: %v", err)
+		return
+	}
+}
