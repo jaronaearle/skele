272294a65af400700b08d2503cd3a51565527da2
jaronaearle <jaronaearle@gmail.com>
2022-12-02 22:56:20 -0500

    feat(todo): add command handler file - will become gateway listener

---
+++ internal/handlers/CommandHandler.go
@@ -0,0 +1,4 @@
+package handlers
+
+// CommandHandler endpoint will essentially act as a gateway to other applications.
+// It will accept a data payload with a type property and respond appropriately if the type of payload is known, otherwill will not do anything.
