6013ff90e33ef5ff0d15d35495df2baa8912e9b6
Jaron Earle <jaronaearle@gmail.com>
2022-12-07 23:24:29 -0700

    use new data consts, get avy list wip

--- internal/bot/DiscordBot.go
+++ internal/bot/DiscordBot.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/bwmarrin/discordgo"
 )
+
 type DiscordBot struct {
 	Session *discordgo.Session
 }
--- internal/crawlers/AvyCrawler.go
+++ internal/crawlers/AvyCrawler.go
@@ -14,18 +14,59 @@ var (
 
 type AvyCrawler struct {
 	Collector *colly.Collector
-	URL       string
 }
 
 func NewAvyCrawler(collector *colly.Collector) *AvyCrawler {
-	url := "https://utahavalanchecenter.org/forecast/salt-lake"
 	return &AvyCrawler{
 		Collector: collector,
-		URL:       url,
 	}
 }
 
 func (ac *AvyCrawler) GetReport() (rp data.AvyReport, err error) {
+	configureCrawler(ac)
+
+	ac.Collector.OnHTML(".view-content", func(e *colly.HTMLElement) {
+		fmt.Println("attempting to crawl...")
+		rp.Date = e.ChildText(".text_01 .nowrap")
+		rp.Details = e.ChildText(".text_03")
+		rp.ImageUrl = e.ChildAttr(".compass-width", "src")
+
+		fmt.Printf("\n\nDate: %v\nDetails: %v\nImgUrl: %v\n\n", rp.Date, rp.Details, rp.ImageUrl)
+	})
+
+	url := fmt.Sprintf("%s%s", data.AvyUrlPaths.BaseUrl, data.AvyUrlPaths.Forecast)
+	ac.Collector.Visit(url)
+
+	return
+}
+
+func (ac *AvyCrawler) GetTodaysAvyList() (av data.AvyList, err error) {
+	configureCrawler(ac)
+
+	now := time.Now()
+	// today := fmt.Sprintf("%d/%d/%d", now.Month(), now.Day(), now.Year())
+	today := fmt.Sprintf("%d/%d/%d", now.Month(), 6, now.Year())
+	fmt.Println(today)
+
+	// var dates []string
+
+	ac.Collector.OnHTML(".view-content", func(e *colly.HTMLElement) {
+		fmt.Println("attempting to crawl...")
+
+		e.ForEach(".date-display-single", func(_ int, el *colly.HTMLElement) {
+			if el.Text == today {
+				fmt.Println("TODAY")
+			}
+		})
+	})
+
+	url := fmt.Sprintf("%s%s", data.AvyUrlPaths.BaseUrl, data.AvyUrlPaths.Avalanches)
+	ac.Collector.Visit(url)
+
+	return
+}
+
+func configureCrawler(ac *AvyCrawler) {
 	ac.Collector.Limit(&colly.LimitRule{
 		Parallelism: 1, RandomDelay: 7 * time.Second,
 	})
@@ -43,15 +84,4 @@ func (ac *AvyCrawler) GetReport() (rp data.AvyReport, err error) {
 	ac.Collector.OnError(func(r *colly.Response, err error) {
 		fmt.Println("Response error: ", err)
 	})
-
-	ac.Collector.OnHTML(".view-content", func(e *colly.HTMLElement) {
-		fmt.Println("attempting to crawl...")
-		rp.Date = e.ChildText(".text_01 .nowrap")
-		rp.Details = e.ChildText(".text_03")
-		rp.ImageUrl = e.ChildAttr(".compass-width", "src")
-
-		fmt.Printf("\n\nDate: %v\nDetails: %v\nImgUrl: %v\n\n", rp.Date, rp.Details, rp.ImageUrl)
-	})
-	ac.Collector.Visit(ac.URL)
-	return
 }
