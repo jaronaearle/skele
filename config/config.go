29b142c4d189697ea5a38addd84b49f7cfa78a34
Jaron Earle <jaronaearle@gmail.com>
2022-12-18 21:50:08 -0700

    feat: add honeybadger for error monitoring

--- config/config.go
+++ config/config.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/honeybadger-io/honeybadger-go"
 	"github.com/joho/godotenv"
 )
 
@@ -11,26 +12,37 @@ type Config struct {
 	BotToken string
 }
 
-func GetConfig() (config Config, err error) {
+func GetConfig() (cfg Config, err error) {
 	bt, err := getEnvVar("BOT_TOKEN")
 	if err != nil {
-		fmt.Printf("GetConfig: Error loading bot token: %v", err)
+		honeybadger.Notify("GetConfig: Error loading bot token: %v", err)
 		return
 	}
 
+	hb, err := getEnvVar("HONEY_BADGER_API_KEY")
+	if err != nil {
+		honeybadger.Notify("GetConfig: Error loading honeybadger api key: %v", err)
+		return
+	}
+
+	honeybadger.Configure(honeybadger.Configuration{APIKey: hb})
+
 	token := fmt.Sprintf("Bot %s", bt)
-	config = Config{BotToken: token}
+	cfg = Config{BotToken: token}
 
 	return
 }
 
+
+
 func getEnvVar(k string) (v string, err error) {
 	err = godotenv.Load(".env")
 	if err != nil {
-		fmt.Printf("getEnvVar: Error loading env vars from .env: %v\n", err)
+		honeybadger.Notify("getEnvVar: Error loading env vars from .env: %v\n", err)
 		return
 	}
 	v = os.Getenv(k)
 
 	return
 }
+
