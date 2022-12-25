46ab34ffb7510d0883f5b3eed5468bd2096adffc
Jaron Earle <jaronaearle@gmail.com>
2022-12-24 23:47:00 -0700

    stop containers before starting new container && test test

--- main.go
+++ main.go
@@ -95,7 +95,7 @@ func startCron(pCtx context.Context, h Handlers, exit context.CancelFunc) {
 		h.AvyCrawlerHandler.SendTodaysAvyList()
 	})
 
-	s.Every(1).Days().At("23:45").Do(func() {
+	s.Every(1).Days().At("23:50").Do(func() {
 		h.AvyCrawlerHandler.SendTodaysAvyList()
 	})
 
