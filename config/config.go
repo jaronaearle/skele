633b7d4e163b333a49848fecfdeba14f25d330e4
Jaron Earle <jaronaearle@gmail.com>
2023-01-02 21:06:33 -0700

    refactor: improve errors in config'

--- config/config.go
+++ config/config.go
@@ -16,27 +16,23 @@ type Config struct {
 func GetConfig() (cfg Config, err error) {
 	bt, err := getEnvVar("BOT_TOKEN")
 	if err != nil {
-		honeybadger.Notify("GetConfig: Error loading bot token: %v", err)
 		return
-	}
+		}
 
 	hb, err := getEnvVar("HONEY_BADGER_API_KEY")
 	if err != nil {
-		honeybadger.Notify("GetConfig: Error loading honeybadger api key: %v", err)
 		return
-	}
+		}
 
 	pt, err := getEnvVar("PAPER_TRAIL_HOST")
 	if err != nil {
-		honeybadger.Notify("GetConfig: Error loading paper trail host: %v", err)
 		return
-	}
+		}
 
-	// Configures connection to honeybadger
 	honeybadger.Configure(honeybadger.Configuration{APIKey: hb})
 
-	token := fmt.Sprintf("Bot %s", bt)
-	cfg = Config{BotToken: token, PaperTrailHost: pt}
+	botToken := fmt.Sprintf("Bot %s", bt)
+	cfg = Config{BotToken: botToken, PaperTrailHost: pt}
 
 	return
 }
@@ -44,9 +40,8 @@ func GetConfig() (cfg Config, err error) {
 func getEnvVar(k string) (v string, err error) {
 	err = godotenv.Load(".env")
 	if err != nil {
-		honeybadger.Notify("getEnvVar: Error loading env vars from .env: %v\n", err)
-		return
-	}
+		err = fmt.Errorf("getEnvVar: Error pulling %v from .env", k)
+			}
 	v = os.Getenv(k)
 
 	return
